13b9e97b187e65693112e9714463bf535774bb83
SlowMan <lai.yongbin@gmail.com>
2014-06-23 15:54:04 +0800

    update

--- core/src/main/java/parser/dex/CodeAdapter.java
+++ core/src/main/java/parser/dex/CodeAdapter.java
@@ -6,11 +6,6 @@ import com.googlecode.dex2jar.util.DumpDexCodeAdapter;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 
-/**
- * Created by SlowMan on 14-6-4.
- * 1、解析方法的具體內容。
- * 2、解析OP_CONST_STRING 對應的字符串。
- */
 public class CodeAdapter extends DumpDexCodeAdapter {
     DexClass dexClass;
     Method method;
@@ -22,17 +17,6 @@ public class CodeAdapter extends DumpDexCodeAdapter {
         this.method = method;
         this.writer = writer;
     }
-
-
-    // FIXME 這部分代碼會影響對方法的收集，丟失字符串部分。需要手工從方法體內，提取字符串。
-//    @Override
-//    public void visitConstStmt(int opcode, int toReg, Object value, int xt) {
-//        switch (opcode) {
-//            case OP_CONST_STRING:
-//                dexClass.stringData.add(XMLString.escape(value));
-//                break;
-//        }
-//    }
 
     @Override
     public void visitEnd() {
